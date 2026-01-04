c48a0053e07c0c1d60bccb50325cd47630e6254d
Ben Du <longendu@yahoo.com>
2026-01-04 12:44:35 -0800

    refactor and fix lint issues

--- dockeree/builder.py
+++ dockeree/builder.py
@@ -1,46 +1,20 @@
 """Docker related utils."""
 
 from __future__ import annotations
-from typing import Callable
-from dataclasses import dataclass
-import tempfile
 from pathlib import Path
-import time
+from typing import Callable
+from collections import deque
 import datetime
-from collections import deque, namedtuple
-import shutil
 import subprocess as sp
+import time
 import yaml
 from loguru import logger
 import docker
 import networkx as nx
 import pygit2
-import pytest
-
-
-def retry(task: Callable, times: int = 3, wait_seconds: float = 60):
-    """Retry a Docker API on failure (for a few times).
-    :param task: The task to run.
-    :param times: The total number of times to retry.
-    :param wait_seconds: The number of seconds to wait before retrying.
-    :return: The return result of the task.
-    """
-    for _ in range(1, times):
-        try:
-            return task()
-        except Exception:
-            time.sleep(wait_seconds)
-    return task()
-
-
-def tag_date(tag: str) -> str:
-    """Suffix a tag with the current date as a 6-digit string.
-
-    :param tag: A tag of Docker image.
-    :return: A new tag.
-    """
-    mmddhh = datetime.datetime.now().strftime("%m%d%H")
-    return mmddhh if tag in ("", "latest") else f"{tag}_{mmddhh}"
+from .utils import branch_to_tag, get_docker_builder, reg_tag, DockerActionResult
+from .node import Node
+from .image import DockerImage
 
 
 def _push_image_timing(repo: str, tag: str) -> DockerActionResult:
@@ -70,348 +44,29 @@ def _push_image_timing(repo: str, tag: str) -> DockerActionResult:
         )
 
 
-def _ignore_socket(dir_, files):
-    dir_ = Path(dir_)
-    return [file for file in files if (dir_ / file).is_socket()]
-
-
-def branch_to_tag(branch: str) -> str:
-    """Convert a branch to its corresponding Docker image tag.
-
-    :param branch: A branch name.
-    :return: The Docker image tag corresponding to the branch.
+def retry(task: Callable, times: int = 3, wait_seconds: float = 60):
+    """Retry a Docker API on failure (for a few times).
+    :param task: The task to run.
+    :param times: The total number of times to retry.
+    :param wait_seconds: The number of seconds to wait before retrying.
+    :return: The return result of the task.
     """
-    if branch in ("master", "main"):
-        return "latest"
-    if branch == "dev":
-        return "next"
-    return branch
-
-
-def _reg_tag(tag: None | str | list[str], branch: str):
-    if tag is None:
-        tag = branch_to_tag(branch)
-    elif tag == "":
-        tag = "latest"
-    if isinstance(tag, str):
-        tag = [tag]
-    return tag
-
-
-def _get_docker_builder() -> str:
-    docker = "docker"
-    if shutil.which(docker):
-        return docker
-    kaniko = "/kaniko/executor"
-    if Path(kaniko).is_file():
-        return kaniko
-    return ""
-
-
-@dataclass(frozen=True)
-class Node:
-    """A class similar to DockerImage for simplifying code."""
-
-    git_url: str
-    branch: str
-
-    def __str__(self):
-        rindex = self.git_url.rindex("/")
-        # HTTP urls, e.g., https://github.com/dclong/docker-jupyterhub-ds
-        index = self.git_url.rfind("/", 0, rindex)
-        if index < 0:
-            # SSH urls, e.g., git@github.com:dclong/docker-jupyterhub-ds
-            index = self.git_url.rindex(":", 0, rindex)
-        return self.git_url[(index + 1) :] + f"<{self.branch}>"
-
-
-DockerActionResult = namedtuple(
-    "DockerActionResult", ["succeed", "err_msg", "image", "tag", "action", "seconds"]
-)
-
-
-class DockerImage:
-    """Class representing a Docker Image."""
-
-    DOCKERFILE = "Dockerfile"
-
-    def __init__(
-        self,
-        git_url: str,
-        branch: str = "dev",
-        branch_fallback: str = "dev",
-        repo_path: dict[str, str] | None = None,
-        root_image_name: str = "",
-    ):
-        """Initialize a DockerImage object.
-
-        :param git_url: URL of the remote Git repository.
-        :param branch: The branch of the GitHub repository to use.
-        """
-        self._git_url = git_url[:-4] if git_url.endswith(".git") else git_url
-        self._branch = branch
-        self._branch_fallback = branch_fallback
-        self._repo_path = {} if repo_path is None else repo_path
-        self._path = None
-        self._name = ""
-        self._base_image = ""
-        self._root_image_name = root_image_name.split(":")[0] + ":"
-        self._git_url_base = ""
-
-    def is_root(self) -> bool:
-        """Check whether this DockerImage is a root DockerImage."""
-        return (
-            self._base_image.startswith(self._root_image_name) or not self._git_url_base
-        )
-
-    def clone_repo(self) -> None:
-        """Clone the Git repository to a local directory."""
-        if self._path:
-            return
-        if self._git_url in self._repo_path:
-            self._path = self._repo_path[self._git_url]
-            repo = pygit2.Repository(self._path)
-            logger.info(
-                "{} has already been cloned into {} previously.",
-                self._git_url,
-                self._path,
-            )
-        else:
-            self._path = Path(tempfile.mkdtemp())
-            logger.info("Cloning {} into {}", self._git_url, self._path)
-            repo = pygit2.clone_repository(self._git_url, self._path)
-            self._repo_path[self._git_url] = self._path
-        self._checkout_branch(repo)
-        self._parse_dockerfile()
-
-    def _checkout_branch(self, repo) -> None:
-        """Checkout the branch self._branch from repo if the branch exists.
-        Otherwise, create a new branch named self._branch in repo and checkout it.
-        """
-        repo.reset(repo.head.peel().id, pygit2.GIT_RESET_HARD)  # pylint: disable=E1101
-        if repo.branches.get(self._branch) is None:
-            for ref in [
-                f"refs/remotes/origin/{self._branch}",
-                f"refs/heads/{self._branch_fallback}",
-                f"refs/remotes/origin/{self._branch_fallback}",
-            ]:
-                ref = repo.references.get(ref)
-                if ref:
-                    repo.create_branch(self._branch, ref.peel())
-                    break
-        repo.checkout(f"refs/heads/{self._branch}")
-
-    def _parse_dockerfile(self):
-        dockerfile = self._path / DockerImage.DOCKERFILE
-        with dockerfile.open() as fin:
-            for line in fin:
-                if line.startswith("# NAME:"):
-                    self._name = line[7:].strip()
-                    logger.info("This image name: {}", self._name)
-                elif line.startswith("FROM "):
-                    self._base_image = line[5:].strip()
-                    if ":" not in self._base_image:
-                        self._base_image += ":latest"
-                    logger.info("Base image name: {}", self._base_image)
-                elif line.startswith("# GIT:"):
-                    self._git_url_base = line[6:].strip()
-                    logger.info("Base image URL: {}", self._git_url_base)
-        if not self._name:
-            raise LookupError("The name tag '# NAME:' is not found in the Dockerfile!")
-        if not self._base_image:
-            raise LookupError("The FROM line is not found in the Dockerfile!")
-
-    def get_deps(self, repo_branch) -> deque[DockerImage]:
-        """Get all dependencies of this DockerImage in order.
-
-        :param repo_branch: A set-like collection containing tuples of (git_url, branch).
-        :return: A deque containing dependency images.
-        """
-        self.clone_repo()
-        deps = deque([self])
-        obj = self
-        while (
-            obj._git_url_base,  # pylint: disable=W0212
-            obj._branch,  # pylint: disable=W0212
-        ) not in repo_branch:
-            if obj.is_root():
-                break
-            obj = obj.base_image()
-            deps.appendleft(obj)
-        return deps
-
-    def base_image(self) -> DockerImage:
-        """Get the base DockerImage of this DockerImage."""
-        image = DockerImage(
-            git_url=self._git_url_base,
-            branch=self._branch,
-            branch_fallback=self._branch_fallback,
-            repo_path=self._repo_path,
-            root_image_name=self._root_image_name,
-        )
-        image.clone_repo()
-        return image
-
-    def _copy_ssh(self, copy_ssh_to: str):
-        if copy_ssh_to:
-            ssh_src = Path.home() / ".ssh"
-            if not ssh_src.is_dir():
-                logger.warning("~/.ssh does NOT exists!")
-                return
-            ssh_dst = self._path / copy_ssh_to
-            try:
-                shutil.rmtree(ssh_dst)
-            except FileNotFoundError:
-                pass
-            shutil.copytree(ssh_src, ssh_dst, ignore=_ignore_socket)
-            logger.info("~/.ssh has been copied to {}", ssh_dst)
-
-    def build(
-        self,
-        tags: None | str | list[str] = None,
-        copy_ssh_to: str = "",
-        builder: str = _get_docker_builder(),
-    ) -> DockerActionResult:
-        """Build the Docker image.
-
-        :param tags: The tags of the Docker image to build.
-            If None (default), then it is determined by the branch name.
-            When the branch is master the "latest" tag is used,
-            otherwise the next tag is used.
-            If an empty string is specifed for tags,
-            it is also treated as the latest tag.
-        :param copy_ssh_to: If True, SSH keys are copied into a directory named ssh
-            under the current local Git repository.
-        :param builder: The tool to use to build Docker images.
-        :return: A tuple of the format (image_name_built, tag_built, time_taken, "build").
-        """
-        time_begin = time.perf_counter_ns()
-        self.clone_repo()
-        self._copy_ssh(copy_ssh_to)
-        tags = _reg_tag(tags, self._branch)
-        tag0 = tags[0]
-        image_tag = f"{self._name}:{tag0}"
-        logger.info("Building the Docker image {} ...", image_tag)
-        self._update_base_tag(tag0)
-        images = docker.from_env().images
+    for _ in range(1, times):
         try:
-            images.remove(image_tag, force=True)
+            return task()
         except Exception:
-            pass
-        try:
-            if builder == "docker":
-                for msg in docker.APIClient(
-                    base_url="unix://var/run/docker.sock"
-                ).build(
-                    path=str(self._path),
-                    tag=image_tag,
-                    rm=True,
-                    pull=self.is_root(),
-                    cache_from=None,
-                    decode=True,
-                ):
-                    if "stream" in msg:
-                        print(f"[{image_tag}] {msg['stream']}", end="")
-                # add additional tags for the image
-                image = images.get(image_tag)
-                for tag in tags[1:]:
-                    image.tag(self._name, tag, force=True)
-            elif builder == "/kaniko/executor":
-                dests = " ".join(f"-d {self._name}:{tag}" for tag in tags)
-                cmd = f"/kaniko/executor --cleanup -c {self._path} {dests}"
-                sp.run(cmd, shell=True, check=True)
-            elif builder == "":
-                raise ValueError("Please provide a valid Docker builder!")
-            else:
-                raise NotImplementedError(
-                    f"The docker builder {builder} is not supported yet!"
-                )
-        except docker.errors.BuildError as err:
-            return DockerActionResult(
-                succeed=False,
-                err_msg="\n".join(
-                    line.get("stream", line.get("error")) for line in err.build_log
-                ),
-                image=self._name,
-                tag="",
-                action="build",
-                seconds=(time.perf_counter_ns() - time_begin) / 1e9,
-            )
-        except docker.errors.ImageNotFound:
-            return DockerActionResult(
-                succeed=False,
-                err_msg="",
-                image=self._name,
-                tag="",
-                action="build",
-                seconds=(time.perf_counter_ns() - time_begin) / 1e9,
-            )
-        finally:
-            self._remove_ssh(copy_ssh_to)
-        if self._test_built_image():
-            return DockerActionResult(
-                succeed=True,
-                err_msg="",
-                image=self._name,
-                tag=tag0,
-                action="build",
-                seconds=(time.perf_counter_ns() - time_begin) / 1e9,
-            )
-        return DockerActionResult(
-            succeed=False,
-            err_msg="Built image failed to pass tests.",
-            image=self._name,
-            tag=tag0,
-            action="build",
-            seconds=(time.perf_counter_ns() - time_begin) / 1e9,
-        )
+            time.sleep(wait_seconds)
+    return task()
 
-    def _test_built_image(self) -> bool:
-        code = pytest.main([str(self._path)])
-        return code in (pytest.ExitCode.OK, pytest.ExitCode.NO_TESTS_COLLECTED, 0)
 
-    def _remove_ssh(self, copy_ssh_to: str):
-        if copy_ssh_to:
-            try:
-                shutil.rmtree(self._path / copy_ssh_to)
-            except FileNotFoundError:
-                pass
+def tag_date(tag: str) -> str:
+    """Suffix a tag with the current date as a 6-digit string.
 
-    def _update_base_tag(self, tag_build: str) -> None:
-        if not self._git_url_base:  # self is a root image
-            return
-        dockerfile = self._path / DockerImage.DOCKERFILE
-        with dockerfile.open() as fin:
-            lines = fin.readlines()
-        for idx, line in enumerate(lines):
-            if line.startswith("FROM "):
-                lines[idx] = line[: line.rfind(":")] + f":{tag_build}\n"
-                break
-        with dockerfile.open("w") as fout:
-            fout.writelines(lines)
-
-    def node(self):
-        """Convert this DockerImage to a Node."""
-        return Node(
-            git_url=self._git_url,
-            branch=self._branch,
-        )
-
-    def base_node(self):
-        """Convert the base image of this DockerImage to a Node."""
-        return self.base_image().node()
-
-    def docker_servers(self) -> set[str]:
-        """Get 3rd-party Docker image hosts associated with this DockerImage and its base DockerImage.
-
-        :return: A set of 3rdd-party Docker image hosts.
-        """
-        servers = set()
-        if self._base_image.count("/") > 1:
-            servers.add(self._base_image.split("/", maxsplit=1)[0])
-        if self._name.count("/") > 1:
-            servers.add(self._name.split("/", maxsplit=1)[0])
-        return servers
+    :param tag: A tag of Docker image.
+    :return: A new tag.
+    """
+    mmddhh = datetime.datetime.now().strftime("%m%d%H")
+    return mmddhh if tag in ("", "latest") else f"{tag}_{mmddhh}"
 
 
 class DockerImageBuilderError(Exception):
@@ -425,7 +80,7 @@ class DockerImageBuilder:
         self,
         branch_urls: dict[str, dict[str, str]] | str | Path,
         branch_fallback: str = "dev",
-        builder: str = _get_docker_builder(),
+        builder: str = get_docker_builder(),
     ):
         if isinstance(branch_urls, (str, Path)):
             with open(branch_urls, "r", encoding="utf-8") as fin:
@@ -616,7 +271,7 @@ class DockerImageBuilder:
     def _build_images_graph(
         self,
         node,
-        tag_build: str,
+        tag_build: str | None,
         copy_ssh_to: str,
         push: bool,
         remove: bool,
@@ -680,7 +335,7 @@ class DockerImageBuilder:
     #        action_time.append(tas)
 
     def _gen_add_tags(self, tag_build, node) -> list:
-        tag_build = _reg_tag(tag_build, node.branch)[0]
+        tag_build = reg_tag(tag_build, node.branch)[0]
         tags = {
             tag_build: None,
             tag_date(tag_build): None,
